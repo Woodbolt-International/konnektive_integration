5c68db5acae422f54fa80bd2b896ef63473cc606
Andrea Pavoni <andrea.pavoni@gmail.com>
2015-06-08 10:48:48 +0200

    adds shipping and billing address to order

--- lib/wombat_data_adapter.rb
+++ lib/wombat_data_adapter.rb
@@ -22,18 +22,53 @@ class WombatDataAdapter
       currency: "USD",
       placed_on: order['dateCreated'],
       updated_at: order['dateCreated'],
-      line_items: order['items'].keys.map { |id|
-        item = order['items'][id]
+      line_items: line_items(order),
+      shipping_address: shipping_address,
+      billing_address: billing_address
+    }
+  end
 
-        {
-          id: item['orderItemId'],
-          product_id: item['productId'],
-          name: item['name'],
-          quantity: item['qty'],
-          price: item['price'],
-          sku: item['productId']
-        }
+  # helpers
+  def line_items(order)
+    order['items'].keys.map do |id|
+      item = order['items'][id]
+
+      {
+        id: item['orderItemId'],
+        product_id: item['productId'],
+        name: item['name'],
+        quantity: item['qty'],
+        price: item['price'],
+        sku: item['productId']
       }
+    end
+  end
+
+  def billing_address(order)
+    {
+      firstname: order['firstName'],
+      lastname: order['lastName'],
+      address1: order['address1'],
+      address2: order['address2'],
+      city: order['city'],
+      state: order['state'],
+      country: order['country'],
+      zipcode: order['postalCode'],
+      phone: order['phoneNumber']
+    }
+  end
+
+  def shipping_address(order)
+    {
+      firstname: order['shipFirstName'],
+      lastname: order['shipLastName'],
+      address1: order['shipAddress1'],
+      address2: order['shipAddress2'],
+      city: order['shipCity'],
+      state: order['shipState'],
+      country: order['shipCountry'],
+      zipcode: order['shipPostalCode'],
+      phone: order['phoneNumber']
     }
   end
 end
